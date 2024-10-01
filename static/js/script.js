7a00328c0085e6da142fad248461de36ba1e1cc0
GabrielePugliese <163171821+GabrielePugliese@users.noreply.github.com>
2024-10-01 19:09:46 +0200

    ultimazione

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,31 @@
-function numRandom(min, max){
-    let num = Math.random
+let tent = 0
+let num = 0
 
-    return Math.floor(num)
+function numRandom(max){
+    num = Math.floor(Math.random() * max)
+
+    return num;
 }
+numRandom(100)
 
-numRandomInt(1, 100)
+while (tent < 5) {
+    let indovin = prompt("Indovina il numero!")
+    while (indovin === null || isNaN(indovin)) {
+        indovin = prompt("Indovina il numero!")
+    }
+    tent++
+    indovin = parseInt(indovin)
+
+    if (indovin == num) {
+        console.log("Hai vinto!")
+        tent = 5
+    } else if (indovin < num) {
+        console.log("Numero digitato: " + indovin, "/ Numero minore a quello nascosto. Tentativi rimasti: " + (5 - tent))
+    } else {
+        console.log("Numero digitato: " + indovin, "/ Numero maggiore a quello nascosto. Tentativi rimasti: " + (5 - tent))
+    }
+}
+alert("Hai perso! Il numero nascosto era:  " + num)
+
+
+
